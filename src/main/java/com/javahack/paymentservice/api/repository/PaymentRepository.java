7543232441f942986c38cdd3d9405fba0655e1e6
Punit Anand <puneetanand15@gmail.com>
2022-01-04 21:58:02 +0530

    Complete Payment Service

---
+++ src/main/java/com/javahack/paymentservice/api/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.javahack.paymentservice.api.repository;
+
+import com.javahack.paymentservice.api.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment,Integer> {
+}
---
+++ src/main/java/com/javahack/paymentservice/api/service/PaymentService.java
@@ -0,0 +1,27 @@
+package com.javahack.paymentservice.api.service;
+
+import com.javahack.paymentservice.api.entity.Payment;
+import com.javahack.paymentservice.api.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+import java.util.UUID;
+
+@Service
+public class PaymentService {
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    public Payment doPayment(Payment payment) {
+        payment.setPaymentStatus(paymentProcessing());
+        payment.setTransactionId(UUID.randomUUID().toString());
+        return paymentRepository.save(payment);
+    }
+
+    public String paymentProcessing(){
+        //api should be 3rd party payment gateway: Paytm , paypal ...
+        return new Random().nextBoolean()?"success":"failure";
+    }
+}
